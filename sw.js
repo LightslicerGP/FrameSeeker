9f0a48071871a3ee122c93083b6955bbab0251be
LightslicerGP <Lightslicerandpuzzlegamer@gmail.com>
2025-09-06 21:52:19 -0400

    added scaled icons, and other stuff

--- sw.js
+++ sw.js
@@ -1,3 +1,50 @@
+// This is the "Offline page" service worker
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+const CACHE = "pwabuilder-page";
+
+const offlineFallbackPage = "index.html";
+
+self.addEventListener("message", (event) => {
+    if (event.data && event.data.type === "SKIP_WAITING") {
+        self.skipWaiting();
+    }
+});
+
+self.addEventListener('install', async (event) => {
+    event.waitUntil(
+        caches.open(CACHE)
+            .then((cache) => cache.add(offlineFallbackPage))
+    );
+});
+
+if (workbox.navigationPreload.isSupported()) {
+    workbox.navigationPreload.enable();
+}
+
+self.addEventListener('fetch', (event) => {
+    if (event.request.mode === 'navigate') {
+        event.respondWith((async () => {
+            try {
+                const preloadResp = await event.preloadResponse;
+
+                if (preloadResp) {
+                    return preloadResp;
+                }
+
+                const networkResp = await fetch(event.request);
+                return networkResp;
+            } catch (error) {
+
+                const cache = await caches.open(CACHE);
+                const cachedResp = await cache.match(offlineFallbackPage);
+                return cachedResp;
+            }
+        })());
+    }
+});
+
 self.addEventListener("install", event => {
     self.skipWaiting();
 });
@@ -10,7 +57,7 @@ let lastSharePayload = null;
 
 self.addEventListener('fetch', event => {
     const url = new URL(event.request.url);
-    const isShareTarget = url.pathname.endsWith('/frameseeker-share') || url.pathname.endsWith('frameseeker-share');
+    const isShareTarget = url.pathname.endsWith('/videoIncoming') || url.pathname.endsWith('videoIncoming');
     if (isShareTarget && event.request.method === 'POST') {
         event.respondWith(handleShare(event.request));
         return;
